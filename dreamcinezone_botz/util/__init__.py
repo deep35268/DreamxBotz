92f54d2ddbe2bfc1c8e5c3769dfef1888ffa61ee
deep35268 <Deepx4.3.8.1@gmail.com>
2026-05-24 03:13:26 +0530

    Update __init__.py

--- dreamcinezone_botz/util/__init__.py
+++ dreamcinezone_botz/util/__init__.py
@@ -1,1 +1,9 @@
-# Made with love by HP
+"""
+DreamxBotz Utils Package
+"""
+
+# Import poster functions
+from .poster import get_poster_with_rating
+
+__version__ = "1.0"
+__all__ = ['get_poster_with_rating']
